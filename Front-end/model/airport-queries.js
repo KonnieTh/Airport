21f06a7513f1773a429a7aa117f04c82690c269f
GiannisKat123 <97844048+GiannisKat123@users.noreply.github.com>
2022-06-01 10:50:02 +0300

    Add files via upload

--- Front-end/model/airport-queries.js
+++ Front-end/model/airport-queries.js
@@ -64,35 +64,117 @@ async function deleteAirline(airline,callback){
     }
 }
 
-// async function insertAirline(airline,callback){
-//     const sql = `insert into "Airline" ("airline_name","IATA","ICAO","telephone","email","gate_code") VALUES('${}','${}','${}','${}','${}','${}')`;
-//     try{
-//         const client = await connect();
-//         const res = await client.query(sql);
-//         await client.release();
-//         callback(null,res.rows);
-//     }
-//     catch(err){
-//         callback(err,null);
-//     }
-// }
+async function insertAirline(airline,callback){
+    const sql1 = `select * from "Airline" where "airline_name"='${airline.name}' or "IATA" = '${airline.iata}'`;
+    try{
+        const client = await connect();
+        const res = await client.query(sql1);
+        await client.release();
+        if(res.rows.length>0){
+            console.log("Η αεροπορική εταιρεία που πήγες να προσθέσεις ήδη υπάρχει! Μπορείς να αλλάξεις τα στοιχεία της πατώντας επεξεργασία στην συγκεκριμένη αεροπορική εταιρεία!");
+        }   
+        else{
+            const sql2 = `insert into "Airline" ("airline_ID","airline_name","IATA","ICAO","telephone","email","gate_code") VALUES((select max("airline_ID")+1 from "Airline"),'${airline.name}','${airline.iata}','${airline.icao}','${airline.telephone}','${airline.email}', (select "gate_ID" from "Gate" where "terminal"='${airline.terminal}' and "gate_name"='${airline.gate}' and "gate_number" = '${airline.gate_number}'))`;
+            try{
+                const client = await connect();
+                const res1 = await client.query(sql2);
+                await client.release();
+                callback(null,res1.rows);
+            }
+            catch(err){
+                callback(err,null);
+            }
+        }
+    }
+    catch(err){
+        callback(err,null);
+    }
+}
 
-// async function editGeneralInfo(text,callback){
-//     const sql = ;
-//     try{
-//         const client = await connect();
-//         const res = await client.query(sql);
-//         await client.release();
-//         callback(null,res.rows);
-//     }
-//     catch(err){
-//         callback(err,null);
-//     }
-// }
+async function insertText(textTitle,callback){
+    const sql = `select * from "General_info" where "title"='${textTitle}' `;
+    try{
+        const client = await connect();
+        const res = await client.query(sql);
+        await client.release();
+        console.log(res.rows)
+        callback(null,res.rows);
+    }
+    catch(err){
+        callback(err,null);
+    }
+}
 
-export{getAirlinebyletter,editAirline,deleteAirline}
 
-// ,editAirline,deleteAirline,insertAirline,editGeneralInfo
+async function editText(text,callback){
+    const sql = `update "General_info" set "description"='${text.keimeno}' where "title"='${text.titlos}' `;
+    try{
+        const client = await connect();
+        const res = await client.query(sql);
+        await client.release();
+        console.log(res.rows)
+        callback(null,res.rows);
+    }
+    catch(err){
+        callback(err,null);
+    }
+}
+
+async function createAnnouncement(text,callback){
+    const sql1 = 'select "announcement_ID" from "Announcement"'
+    try{
+        const client = await connect();
+        const res = await client.query(sql1);
+        await client.release();
+        let sql;
+        if (res.rows.length>0){
+            sql = `insert into "Announcement" ("announcement_ID","username","theme","ann_text","ann_date","ann_time","priority") VALUES((select max("announcement_ID")+1 from "Announcement"),'test','${text.titlos}','${text.keimeno}','${text.date}','${text.time}','${text.priority}') `;
+            try{
+                const client = await connect();
+                const res = await client.query(sql);
+                await client.release();
+                console.log(res.rows)
+                callback(null,res.rows);
+            }
+            catch(err){
+                callback(err,null);
+            }
+        }
+        else{
+            sql = `insert into "Announcement" ("announcement_ID","username","theme","ann_text","ann_date","ann_time","priority") VALUES(0,'test','${text.titlos}','${text.keimeno}','${text.date}','${text.time}','${text.priority}') `;
+            try{
+                const client = await connect();
+                const res = await client.query(sql);
+                await client.release();
+                console.log(res.rows)
+                callback(null,res.rows);
+            }
+            catch(err){
+                callback(err,null);
+            }
+        }
+    }
+    catch(err){
+        callback(err,null);
+    }
+}
+
+async function getAnnouncements(callback){
+    const sql = `Select * from "Announcement"`;
+    try{
+        const client = await connect();
+        const res = await client.query(sql);
+        await client.release();
+        console.log(res.rows)
+        callback(null,res.rows);
+    }
+    catch(err){
+        callback(err,null);
+    }
+}
+
+export{getAirlinebyletter,editAirline,deleteAirline,insertAirline,insertText,editText,createAnnouncement,getAnnouncements}
+
 
 
 
