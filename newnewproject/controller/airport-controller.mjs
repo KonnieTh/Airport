addfd5b8f4c3e80cbeec4c8a64cea9ccc576235a
GiannisKat123 <97844048+GiannisKat123@users.noreply.github.com>
2022-06-01 22:13:12 +0300

    Add files via upload

--- newnewproject/controller/airport-controller.mjs
+++ newnewproject/controller/airport-controller.mjs
@@ -28,3 +28,71 @@ export function createAnnouncements(req,res){
     });
 }
 
+export async function getAirlinesByLetter(req,res){
+    model.getAirlinebyletter(letter,(err,rows) => {
+        if(err){
+            return console.error(err.message);
+        }
+        res.render(rows);
+    })
+}
+
+export async function editAirlineInfo(req,res){
+    model.editAirline(airline,(err,data) =>{
+        if(err){
+            return console.error(err.message);
+        }
+        else{
+            res.json(data);
+        }
+    })
+}
+
+export async function deleteAirline(req,res){
+    model.deleteAirline(airline,(err,data) =>{
+        if(err){
+            return console.error(err.message);
+        }
+        else{
+            res.json(data);
+        }
+    })
+}
+  
+export async function AddAirline(req,res){
+    model.insertAirline(airline,(err,data) =>{
+        if(err){
+            return console.error(err.message);
+        }
+        else{
+            res.json(data);
+        }
+    })
+}
+
+export async function getText(req,res){
+    model.insertText(title,(err,data) =>{
+        if(err){
+            return console.error(err.message);
+        }
+        else{
+            res.json(data);
+        }
+    })
+}
+
+export async function editText(req,res){
+    const text = req.body;
+    model.editText(text,(err,data) =>{
+        if(err){
+            return console.error(err.message);
+        }
+        else{
+            res.json(data);
+        }
+    })
+}
+
+
+
+
--- newnewproject/routes/airport-app-routes.mjs
+++ newnewproject/routes/airport-app-routes.mjs
@@ -14,70 +14,85 @@ router.route('/').get((req, res) => { res.redirect('/main-page') });
 router.get('/announcements',(req,res)=>{
     res.render('announcements',{
         style:'announcements.css',
-        script:'announcements.js'
+        script:'announcements.js',
+        layout:'layout'
     })
 })
 
 router.get('/announcements-admin',(req,res)=>{
     res.render('announcements-admin',{
         style:'announcements-admin.css',
-        script:'announcements.js'
+        script:'announcements.js',
+        layout:'layout-admin'
     })
 })
 
 router.get('/main-page',(req,res)=>{
     res.render('main-page',{
         style:'style-main-page.css',
-        script:'main.js'
+        script:'main.js',
+        layout:'layout-main-page'
+        
     })
 })
 
 router.get('/main-page-admin',(req,res)=>{
     res.render('main-page-admin',{
         style:'style-main-page-admin.css',
-        script:'main-admin.js'
+        script:'main-admin.js',
+        layout:'layout-main-page-admin'
     })
 })
 
 router.get('/flights',(req,res)=>{
     res.render('flights',{
         style:'flights.css',
-        script:'flights.js'
+        script:'flights.js',
+        layout:'layout'
     })
 })
 
 router.get('/flights-admin',(req,res)=>{
     res.render('flights-admin',{
         style:'flights-admin.css',
-        script:'flights.js'
+        script:'flights.js',
+        layout:'layout-admin'
     })
 })
 
 router.get('/shops',(req,res)=>{
     res.render('shops',{
         style:'shops.css',
-        script:'shops.js'
+        script:'shops.js',
+        layout:'layout'
     })
 })
 
 router.get('/companies-admin',(req,res)=>{
     res.render('companies-admin',{
         style:'companies-admin.css',
-        script:'companies-admin.js'
+        script:'companies-admin.js',
+        layout:'layout-admin'
+        
     })
 })
 
 router.get('/companies',(req,res)=>{
     res.render('companies',{
         style:'companies.css',
-        script:'companies.js'
+        script:'companies.js',
+        layout:'layout'
     })
 })
 
 
-router.post('/anouncements-admin/create_announcement',appController.createAnnouncements);
-router.get('/announcements-admin/get_announcements/start/:start/limit/:limit',appController.getAnnouncementsRender);
-router.get('/announcements/get_announcements/start/:start/limit/:limit',appController.getAnnouncementsRender);
+router.post('/create_announcement',appController.createAnnouncements);
+router.get('/get_announcements/start/:start/limit/:limit',appController.getAnnouncementsRender);
+router.get('/airlines/filter/:letter',appController.getAirlinesByLetter);
+router.put('/airlines_edit',appController.editAirlineInfo);
+router.delete('/airlines_delete',appController.deleteAirline);
+router.post('/new_airline',appController.AddAirline);
+router.get('/text/:titlos',appController.getText);
+router.put('/edit_text',appController.editText);
 
 export default router;
-
