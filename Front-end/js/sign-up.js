2aa67099765746072603ef3a775cbd72102c4c6c
KonnieTh <70428867+KonnieTh@users.noreply.github.com>
2022-06-04 18:49:12 +0300

    Delete Front-end directory

--- Front-end/js/sign-up.js
+++
@@ -1,9 +0,0 @@
-document.querySelector(".sign-up").addEventListener("click",function(){
-    document.querySelector(".signup").classList.add("active");
-    document.querySelector("body").classList.add("active");
-});
-
-document.querySelector(".signup .close").addEventListener("click",function(){
-    document.querySelector(".signup").classList.remove("active");
-    document.querySelector("body").classList.remove("active");
-});
